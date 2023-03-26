4fc6803985f8293fc4750d9b1598e173caf8b2e5
Ahmad Alhourani <ahmad.alhourani.ite90@gmail.com>
2023-03-26 14:55:42 +0400

    fix scrip api for cors

--- config/scribe.php
+++ config/scribe.php
@@ -191,7 +191,7 @@ return [
          * The base URL for the API tester to use (for example, you can set this to your staging URL).
          * Leave as null to use the current app URL (config(app.url)).
          */
-        'base_url' => null,
+        'base_url' => config('app.url') . ':' . config('app.port'),
 
         /**
          * Fetch a CSRF token before each request, and add it as an X-XSRF-TOKEN header. Needed if you're using Laravel Sanctum.
